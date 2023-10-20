cf9ac1bac8f2255c2aaa97704b186550132d14a6
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-20 20:22:08 -0300

    feat: Implement is_pallindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    # condição de parada
+    # a função para de chamar a si mesma quando chegar ao meio da palavra
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        # se as letras forem iguais,
+        # a função é chamada com os index progredindo, um em cada direção
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return False
