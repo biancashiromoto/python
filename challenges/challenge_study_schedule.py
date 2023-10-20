246c4f1e8061a547cf8e1e6a5064dd6e0e573ff6
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-20 18:15:18 -0300

    feat: Implement function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    # raise NotImplementedError
+    if target_time is None:
+        return None
+    counter = 0
+    for student_permanence in permanence_period:
+        entry_time = student_permanence[0]
+        out_time = student_permanence[1]
+        if (
+            entry_time is None or not isinstance(entry_time, int)
+          ) or (
+            out_time is None or not isinstance(out_time, int)
+          ):
+            return None
+        if (int(entry_time) <= target_time <= int(out_time)):
+            counter += 1
+    return counter
+
+
