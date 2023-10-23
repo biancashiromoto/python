258b2edf5c277cc25a89be9662882e5244a5173e
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-23 18:11:46 -0300

    feat: Implement function is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,67 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_first_string = sort_string(first_string.lower())
+    sorted_second_string = sort_string(second_string.lower())
+    third_element = False
+    if (first_string == "" or second_string == ""):
+        third_element = False
+    elif (sorted_first_string != sorted_second_string):
+        third_element = False
+    else:
+        third_element = True
+    return sorted_first_string, sorted_second_string, third_element
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    # divide o array pela metade e as nomeia left e right
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    # divide os dois arrays novamente em dois, de forma recursiva
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    # retorna o array ordenado
+    return merge(left, right)
+
+
+# recebe dois arrays como parâmetro
+def merge(left, right):
+    # cria um array vazio que vai armazenar os números ordenados
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        # se o elemento left[i] < right[j], ele deve ser adicionado ao result
+        # e o i é incrementado para passarmos para o próximo elemento
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        # caso contrário, o elemento de right deve ser adicionado ao result
+        # e j é incrementado para passarmos para o próximo elemento
+        else:
+            result.append(right[j])
+            j += 1
+
+    # ao final, os elementos de left a partir do index i até o final do array
+    # são adicionados a result
+    result.extend(left[i:])
+    # ao final, os elementos de right a partir do index j até o final do array
+    # são adicionados a result
+    result.extend(right[j:])
+    return result
+
+
+def sort_string(input_string):
+    # converte a string de entrada em uma lista de caracteres
+    char_list = list(input_string)
+    # ordena os caracteres e retorna uma nova lista contendo os caracteres
+    # ordenados
+    sorted_chars = merge_sort(char_list)
+    # concatena os caracteres da lista sorted_charts em uma única string
+    sorted_string = ''.join(sorted_chars)
+    return sorted_string
+
