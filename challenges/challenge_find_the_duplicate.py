c8fa33ffd034a43ced316cdf38b65482cac98b88
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-23 18:50:53 -0300

    feat: Implement function find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # criação de dois sets vazios - sets não permitem números repetidos
+    numbers = set()
+    duplicates = 0
+
+    # itera sobre o array
+    for num in nums:
+        if (isinstance(num, str)) or (num < 0):
+            return False
+        # caso o número esteja nos números do set
+        if num in numbers:
+            # adiciona o número ao set de duplicatas
+            duplicates = num
+        # caso contrário, adiciona aos números que já foram passados
+        else:
+            numbers.add(num)
+
+    if (duplicates == 0) or (len(nums) <= 1) or (isinstance(nums, str)):
+        return False
+    return duplicates
