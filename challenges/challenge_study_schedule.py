3f238b440af6e17c8d6c31e604c5c1fdc1117302
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-24 16:53:22 -0300

    chore: Update description

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,12 @@
 # Desafio 01: Desenvolva uma função que receba como parâmetros uma lista de tuplas contendo o período de permanência de um grupo de pessoas estudantes em um sistema com o horário de entrada em saída (permanence_period) e um horário (target_time). A função deve retornar quantas pessoas estudantes estão ativas no target_time dado, respeitando os seguintes requisitos:
 # - O algoritmo deve utilizar uma solução iterativa
 # - Caso o target_time seja nulo, ou haja alguma entrada inválida em permanence_period, o valor retornado deve ser None
-# Exemplos de lista de tuplas:
+
+# Challenge 01: Design a function that takes a list of tuples representing the time intervals when students are present (permanence_period) and a target time. The function should compute and return the count of active students at the specified target_time. Adhere to the following conditions:
+# - The algorithm must implement an iterative approach.
+# - If the target_time is null or there are any invalid entries in permanence_period, the function should return None.
+
+# Exemplos de lista de tuplas/Tuples list example:
 # [
 #     ([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)], 5, 3),
 #     ([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1, 2),
@@ -9,10 +14,6 @@
 #     ([(1, 1), (2, 2), (3, 3), (4, 4)], 1, 1),
 #     ([(1, 2), (1, 3), (2, 3)], 2, 3),
 # ]
-
-# Challenge 01: Design a function that takes a list of tuples representing the time intervals when students are present (permanence_period) and a target time. The function should compute and return the count of active students at the specified target_time. Adhere to the following conditions:
-# - The algorithm must implement an iterative approach.
-# - If the target_time is null or there are any invalid entries in permanence_period, the function should return None.
 
 
 def study_schedule(permanence_period, target_time):
