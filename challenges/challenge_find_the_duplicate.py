84a87ee3f6cc354210d47cd067d66f04982f6cb1
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-24 17:04:01 -0300

    chore: Add description

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,7 @@
+# Desafio 05: Dado um array nums de números inteiros contendo n + 1 itens, em que cada inteiro está no intervalo [1, n], retorne apenas um número duplicado em nums. Caso não seja passado nenhum valor, ou seja passada uma string, ou não haja valores repetidos, retorne False.
+# Challenge 05: Given an array nums of integers containing n + 1 items, in which each integer is within the interval [1, n], return only a duplicate number in nums. In case no value is passed, or a string is given, or there are no repeated values, return False.
+
+
 def find_duplicate(nums):
     # criação de dois sets vazios - sets não permitem números repetidos
     numbers = set()
